c37c6b6fd4c6f3919d00788ee98b12e47af0c09d
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2021-01-01 17:44:40 -0800

    use var for ecr-repository/personal-website

--- prod/services/ecr-repositories/personal-website/main.tf
+++ prod/services/ecr-repositories/personal-website/main.tf
@@ -15,7 +15,7 @@ terraform {
 
 module "ecr_repository_personal_website" {
   source = "github.com/nickolasteixeira/personal_terraform_modules//modules/aws/ecr-repository?ref=v0.0.3"
-  name = "personal-website"
+  name = var.name
   scan_on_push = var.scan_on_push
   encryption_type = var.encryption_type
   image_tag_mutability = var.image_tag_mutability
